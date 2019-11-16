3fceeacc72191f3d35a842c78be40988d8646847
ywb <nxwsyang@163.com>
2019-11-16 21:05:32 +0800

    测试

--- bigquant/交易回测代码-5只股票.py
+++ bigquant/交易回测代码-5只股票.py
@@ -3,12 +3,11 @@ def bigquant_run(context, data):
     #获取当日日期
     today = data.current_dt
     today_date = data.current_dt.strftime('%Y-%m-%d')
-    
+
     current_stock = [] # 当前持仓  
     current_stopdays_stock = [] # 固定天数卖出列表    
     current_stopwin_stock = []  # 止盈列表
-    current_stoploss_stock = [] # 止损列表
-    cash_for_sell = 0 # 当日卖出现金
+    current_stoploss_stock = [] # 止损列表    
 
     equities = {e.symbol: p for e, p in context.portfolio.positions.items() if p.amount>0}    
     if len(equities)>0:
@@ -21,23 +20,20 @@ def bigquant_run(context, data):
             # 固定天数卖出
             if today-position.last_sale_date>=datetime.timedelta(2) and data.can_trade(symbol) and not context.has_unfinished_sell_order(equities[sid]):
                 context.order_target_percent(symbol, 0)
-                current_stopdays_stock.append(sid)
-                cash_for_sell += position.amount * stock_market_price
+                current_stopdays_stock.append(sid)                
                 continue            
 
-            # 止盈 赚20% 且为可交易状态
-            if stock_market_price/stock_cost-1 >= 0.15  and data.can_trade(symbol) and not context.has_unfinished_sell_order(equities[sid]):
-                context.order_target_percent(symbol,0)
-                current_stopwin_stock.append(sid)
-                cash_for_sell += position.amount * stock_market_price
-                continue
+            # # 止盈 赚15% 且为可交易状态
+            # if stock_market_price/stock_cost-1 >= 0.15  and data.can_trade(symbol) and not context.has_unfinished_sell_order(equities[sid]):
+            #     context.order_target_percent(symbol,0)
+            #     current_stopwin_stock.append(sid)                
+            #     continue
 
-            # 止损 亏5% 且为可交易状态
-            if stock_market_price/stock_cost-1 <= -0.08 and data.can_trade(symbol) and not context.has_unfinished_sell_order(equities[sid]):   
-                context.order_target_percent(symbol,0)     
-                current_stoploss_stock.append(sid)
-                cash_for_sell += position.amount * stock_market_price
-                continue
+            # # 止损 亏8% 且为可交易状态
+            # if stock_market_price/stock_cost-1 <= -0.08 and data.can_trade(symbol) and not context.has_unfinished_sell_order(equities[sid]):   
+            #     context.order_target_percent(symbol,0)     
+            #     current_stoploss_stock.append(sid)                
+            #     continue
             
             current_stock.append(sid)
 
@@ -48,39 +44,38 @@ def bigquant_run(context, data):
     if len(current_stoploss_stock)>0:
         print(today_date,'止损股票列表',current_stoploss_stock)
 
-    cash_for_buy = context.portfolio.cash + cash_for_sell -100 #买入股票 可用现金    
+    cash_for_buy = context.portfolio.cash  #买入股票 可用现金    
+    cash_for_buy_max = context.portfolio.portfolio_value * 0.33 #每天最多买33%仓位
+    cash_for_buy = min(cash_for_buy,cash_for_buy_max)
     #单只股标最大仓位金额
-    max_cash_per_instrument = context.portfolio.portfolio_value * context.max_cash_per_instrument
+    max_cash_per_instrument = context.portfolio.portfolio_value * 0.15 #每只股票最多占用15%仓位    
 
     # 按日期过滤得到今日的预测数据
     ranker_prediction = context.ranker_prediction[context.ranker_prediction.date == today_date]
 
     current_buy_stock = []
-    
+    current_buy_stock_score = []
     for index, row in ranker_prediction.iterrows():
+        if cash_for_buy < 5000:            
+            break
         instrument = row['instrument']
         if instrument in current_stock:
             continue
         if instrument in current_stoploss_stock:
             continue
         if not data.can_trade(context.symbol(instrument)):
-            continue
-        # 评分太低，当天不买
-        #if row['score']<1:
-        #    break
-       
-
-        cash = max_cash_per_instrument
-        breakFlag = False
-        if(cash_for_buy < cash):
-            cash = cash_for_buy
-            breakFlag = True
-        price = data.current(context.symbol(instrument), 'price')  # 最新价格
+            continue        
+        score = row['score']
+        cash = min(max_cash_per_instrument,cash_for_buy)        
+        price = data.current(context.symbol(instrument), 'price')  # 最新价格        
         stock_num = np.floor(cash/price/100)*100  # 向下取整
+        if stock_num == 0:
+            continue
         context.order(context.symbol(instrument), stock_num) # 整百下单
-        cash_for_buy = cash_for_buy - stock_num * price       
+        cash_for_buy = cash_for_buy - stock_num * price               
         current_buy_stock.append(instrument)
-        if(breakFlag):
-            break
+        current_buy_stock_score.append(score)
+        
     if len(current_buy_stock)>0:
         print(today_date,'买入股票',current_buy_stock)
+        print(today_date,'股票得分',current_buy_stock_score)
--- bigquant/自定义函数.py
+++ bigquant/自定义函数.py
@@ -1,1 +1,88 @@
+#标注
+#10日内的最高收益
 
+where(
+min(shift(close, -10),shift(close, -9),shift(close, -8),shift(close, -7),shift(close, -6),shift(close, -5),shift(close, -4),shift(close, -3),shift(close, -2),shift(close, -1))/shift(open, -1) -1 > -0.01
+,max(shift(close, -10),shift(close, -9),shift(close, -8),shift(close, -7),shift(close, -6),shift(close, -5),shift(close, -4),shift(close, -3),shift(close, -2),shift(close, -1))/shift(open, -1)
+,0
+)
+
+# 计算收益：6日收盘价(作为卖出价格)除以明日开盘价(作为买入价格)
+shift(close, -10) / shift(open, -1)
+
+
+
+# pd.set_option('display.max_rows',120)
+#计算nan值有多少个
+df.isna().sum()
+adjust_factor_0               0
+amount_0                      0
+close_0                       0
+date                          0
+high_0                        0
+industry_sw_level1_0          0
+instrument                    0
+low_0                         0
+market_cap_0                  0
+open_0                        0
+volume_0                      0
+Alpha_6                    8257
+Alpha_9                   14751
+Alpha_30                  10360
+Alpha_34                      0
+Alpha_35                   4353
+Alpha_39                   8257
+Alpha_42                      0
+Alpha_43                      0
+Alpha_55                   6169
+Alpha_57                   9313
+Alpha_101                     0
+Alpha_3                    6521
+Alpha_4                    8311
+Alpha_12                   2545
+Alpha_13                    829
+Alpha_14                   3429
+Alpha_15                   8257
+Alpha_16                   3821
+Alpha_17                   3429
+Alpha_27                  24609
+Alpha_45                   3456
+Alpha_48                  19256
+Alpha_51                   8556
+Alpha_83                   8834
+Alpha_31                  19256
+Alpha_32                  32017
+Alpha_41                   8257
+Alpha_44                  41435
+Alpha_46                 118467
+Alpha_52                    829
+Alpha_54                  32880
+Alpha_56                  15877
+Alpha_60                  14227
+Alpha_62                      0
+Alpha_66                 685903
+Alpha_68                      0
+Alpha_73                  20392
+Alpha_78                  48704
+Alpha_84                  35448
+Alpha_86                      0
+Alpha_98                  63177
+Alpha_85                  58051
+Alpha_74                      0
+Alpha_75                      0
+Alpha_77                  53876
+Alpha_81                      0
+Alpha_87                 209386
+Alpha_88                 850768
+Alpha_92                  54858
+Alpha_94                 398818
+Alpha_95                      0
+Alpha_96                3180304
+Alpha_99                      0
+m:amount                      0
+m:high                        0
+m:low                         0
+m:close                       0
+m:open                        0
+label                         0
+dtype: int64
